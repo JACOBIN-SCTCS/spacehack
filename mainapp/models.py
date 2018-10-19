84e1d1961bc533b5e5a5ac8459ad460212233b72
jacob <jacobja3@gmail.com>
2018-10-19 16:50:38 +0530

    Added disaster database

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Disaster(models.Model):
+    dis_id=models.AutoField(primary_key=True)
+    dis_name=models.CharField(max_length=20,default='')
+    dis_description=models.TextField(default='')
+    dis_image=models.ImageField(upload_to='disaster-pic',blank=True)
+
+
+    def __str__(self):
+        return self.dis_name
+
+
+
+
+
